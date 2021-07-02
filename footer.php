188fbf8d21899299dd1b331db269b9dc30b75c16
Sebastián Contreras <62773619+Sebas15897@users.noreply.github.com>
2021-07-01 20:24:27 -0500

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,4 @@
+<footer>
+    <h2>Tienda online Colombiana</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique aliquid soluta in, porro est obcaecati fugit illum earum cupiditate. Accusamus voluptatum deleniti voluptatem fuga totam neque minima harum sit recusandae!</p>
+</footer>
---
+++ nav2.php
@@ -0,0 +1,16 @@
+<nav>
+    <a href="tienda.php" class="nav-logo">Tienda-online</a>
+    <ul class="nav-menu">
+        <li class="nav-item"><a href="tienda.php" class="nav-link">Cerrar sesión</a></li>
+        <li class="nav-item"><a href="productos.php" class="nav-link">Productos</a></li>
+        <li class="nav-item"><a href="dash.php" class="nav-link">Dash_board</a></li>
+    </ul>
+
+    <div class="hamburger">
+        <span class="bar"></span>
+        <span class="bar"></span>
+        <span class="bar"></span>
+    </div>
+
+</nav>
+<script src="js/script.js"></script>
